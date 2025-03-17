a788350b143282bb56f4d417ca2166d39eda35e5
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-03-17 23:00:44 +0100

    Added definition serializer.

---
+++ Scripts/Serialization/DefinitionSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Rusty.ISA
+{
+    /// <summary>
+    /// An utility class that can serialize instruction definitions.
+    /// </summary>
+    public class DefinitionSerializer
+    {
+        /* Public methods. */
+        /// <summary>
+        /// Convert an instruction definition into an XML string.
+        /// </summary>
+        public static string Serialize(InstructionDefinition definition)
+        {
+            InstructionDefinitionDescriptor descriptor = new(definition);
+            return Serialize(descriptor);
+        }
+
+        /// <summary>
+        /// Convert an instruction definition descriptor into an XML string.
+        /// </summary>
+        public static string Serialize(InstructionDefinitionDescriptor descriptor)
+        {
+            XmlSerializer serializer = new(typeof(InstructionDefinitionDescriptor));
+            StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, descriptor);
+            writer.Close();
+            return writer.ToString();
+        }
+    }
+}
